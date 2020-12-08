2eb79b7bf103fe2e5872227ffc490f7223b6b34e
longtenggenssupreme <itwanglinjie@aliyun.com>
2020-12-08 13:23:38 +0800

    add efcore5 vs sqlbulkcopy

--- EFCOREDB/DynamicModelCacheKeyFactory.cs
+++ EFCOREDB/DynamicModelCacheKeyFactory.cs
@@ -1,10 +1,12 @@
-﻿
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 
 
 namespace EFCOREDB
 {
+    /// <summary>
+    /// 分库分表使用
+    /// </summary>
     public class DynamicModelCacheKeyFactory : IModelCacheKeyFactory
     {
         public object Create(DbContext context) => context is DynamicContext dynamicContext ? (context.GetType(), dynamicContext.CreateDateTime) : (object)context.GetType(); 
